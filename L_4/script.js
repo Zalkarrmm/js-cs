4490333beb570f1705d4b652b1838085bff4c1bb
Zalkar Mamatkasymov <zalkar.m@breez.pro>
2024-02-19 12:19:38 +0600

    half-of-lesson

--- L_4/script.js
+++ L_4/script.js
@@ -1,69 +1,121 @@
-const person = {
-  name: "Zalkar",
-  age: 19,
-  job: 'Frontend dev'
+// Theory
+// const person = {
+//   name: "Zalkar",
+//   age: 19,
+//   job: 'Frontend dev'
+// }
+
+// // Object
+
+// const OP = new Proxy(person, { // new Proxy(target, {}:catchers)
+//   get(target, prop) { // get(target, key)
+//     if(!(prop in target)){
+//       return prop
+//         .split('_') // separate a words by underSpace and return as array
+//         .map(p => target[p]) // getting a values from object
+//         .join(' ') // recreate from array to string using ' ' as a spacing
+//     }
+//     return target[prop] // obj[key]
+//   },
+//   set(target, prop, value) {
+//     if (prop in target) {
+//       target[prop] = value
+//     } else {
+//       throw new Error(`No ${prop} in this target`)
+//     }
+//   },
+//   has(target, prop){
+//     return ['age', 'job'].includes(prop)
+//   },
+//   deleteProperty(target, prop){
+//     console.log('Deleting...', prop)
+//     delete target[prop]
+//     return true
+//   }
+// })
+
+// // Function
+
+// const log = text => console.log(`Log: ${text}`)
+
+// const fp = new Proxy(log, {
+//   apply(target, thisArg, args){
+//     console.log('Logging now...')
+//     return target.apply(thisArg, args.toUppercase())
+//   }
+// })
+
+// // Classes
+
+// class Person {
+//   constructor(name, age){
+//     this.name = name
+//     this.age = age
+//   }
+// }
+
+// const PersonProxy = new Proxy(Person, {
+//   construct(target, args){
+//     console.log('Construct...')
+
+//     return new Proxy(new target(...args), {
+//       get(t, prop){
+//         console.log('getting prop', prop)
+//         return t[prop]
+//       }
+//     })
+//   }
+// })
+
+// const p = new PersonProxy('Zalkar', 19)
+
+// Practice
+
+// Wrapper
+
+const withDefaultValue = (target, defaultValue = 0) => {
+  return new Proxy(target, {
+    get: (obj, prop) => (
+      prop in target ? obj[prop] : defaultValue
+    )
+  })
 }
 
-// Object
-
-const OP = new Proxy(person, { // new Proxy(target, {}:catchers)
-  get(target, prop) { // get(target, key)
-    if(!(prop in target)){
-      return prop
-        .split('_') // separate a words by underSpace and return as array
-        .map(p => target[p]) // getting a values from object
-        .join(' ') // recreate from array to string using ' ' as a spacing
-    }
-    return target[prop] // obj[key]
+const position = withDefaultValue(
+  {
+    x: 25,
+    y: 35
   },
-  set(target, prop, value) {
-    if (prop in target) {
-      target[prop] = value
-    } else {
-      throw new Error(`No ${prop} in this target`)
-    }
-  },
-  has(target, prop){
-    return ['age', 'job'].includes(prop)
-  },
-  deleteProperty(target, prop){
-    console.log('Deleting...', prop)
-    delete target[prop]
-    return true
-  }
-})
+  0
+)
 
-// Function
+// console.log(position) // position.y => 35, position.AnyUndefinedKey => 0
 
-const log = text => console.log(`Log: ${text}`)
 
-const fp = new Proxy(log, {
-  apply(target, thisArg, args){
-    console.log('Logging now...')
-    return target.apply(thisArg, args.toUppercase())
-  }
-})
+// Hidden properties
 
-// Classes
-
-class Person {
-  constructor(name, age){
-    this.name = name
-    this.age = age
-  }
+const withHiddenProps = (target, prefix = '_') => { // object and non usable prefix
+  return new Proxy(target, {
+    has: (obj, prop) => (prop in obj) && (!prop.startsWith(prefix)), // returns a key if it doesn't starts with prefix
+    ownKeys: obj => Reflect.ownKeys(obj) // Reflect.ownKeys(obj) returns an array with keys of object (this operation is just like Object.keys(tagetObject))
+      .filter(p => !p.startsWith(prefix)), // just filtering object by prefix when getting keys using ownKeys
+    get: (obj, prop, receiver) => (prop in receiver)  //
+    ? obj[prop]
+    : void 0 // viod 0 === undefined
+  })
 }
 
-const PersonProxy = new Proxy(Person, {
-  construct(target, args){
-    console.log('Construct...')
-
-    return new Proxy(new target(...args), {
-      get(t, prop){
-        console.log('getting prop', prop)
-        return t[prop]
-      }
-    })
-  }
+const data = withHiddenProps({
+  name: 'Vladilen',
+  age: 25,
+  _uid: 'qwerty12345678Aa'
 })
 
-const p = new PersonProxy('Zalkar', 19)
+// Optimization
+
+const userData = [
+  {id: 1, name: 'Alex', job: 'Fullstack', age: 25},
+  {id: 2, name: 'Opra', job: 'Front-end', age: 23},
+  {id: 3, name: 'Andy', job: 'Back-end', age: 19},
+  {id: 4, name: 'Oleg', job: 'DevOps', age: 32 }
+]
