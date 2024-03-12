00cffa2188509ec034879b03da52689c545c9bbe
zalkar mamatkasym <zalkarrmm@gmail.com>
2024-03-12 16:25:06 +0600

    lil repeat an arr methods

--- L_5/script.js
+++ L_5/script.js
@@ -41,3 +41,19 @@ function* iter(n = 10){
 for(let k of iter(6)){
   console.log(k)
 }
+
+
+const pool = [
+  {name: 'Alex', age: 25, budget: 4000},
+  {name: 'William', age: 34, budget: 2434},
+  {name: 'Jane', age: 54, budget: 5444},
+  {name: 'Bony', age: 12, budget: 400544},
+  {name: 'Gonron', age: 44, budget: 90000},
+  {name: 'Ramzy', age: 12, budget: 3434444}
+]
+
+const amount = pool.reduce((total, person) =>  total + person.budget, 0)
+
+const indexOfAlex = pool.findIndex(p => p.name === 'Alex') // returns an index of element
+
+const findAlex = pool.find(p => p.name === 'Alex') // returns an element
